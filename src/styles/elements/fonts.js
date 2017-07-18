3767cff6f2bf2a9bbf174c5303de0dfdd2877523
lzgrzebski <myczek18@gmail.com>
2017-07-19 00:00:20 +0200

    fix path bug in fonts.js

--- src/styles/elements/fonts.js
+++ src/styles/elements/fonts.js
@@ -4,14 +4,14 @@ export default `
     font-family: tms-text;
     font-style: normal;
     font-weight: 500;
-    src: url("static/343A1B_0_0.eot");src: url("static/343A1B_0_0.eot?#iefix") format("embedded-opentype"),url("static/343A1B_0_0.woff2") format("woff2"),url("static/343A1B_0_0.woff") format("woff"),url("static/343A1B_0_0.ttf") format("truetype");
+    src: url('/static/343A1B_0_0.eot');src: url('/static/343A1B_0_0.eot?#iefix') format("embedded-opentype'),url('/static/343A1B_0_0.woff2') format("woff2'),url('/static/343A1B_0_0.woff') format("woff'),url('/static/343A1B_0_0.ttf') format("truetype');
   }
 
   @font-face {
     font-family: tms-header;
     font-weight: normal;
     font-style: normal;
-    src: url('static/343C58_0_0.eot?#iefix') format('embedded-opentype'), url('static/343C58_0_0.woff') format('woff'), url('static/343C58_0_0.ttf')  format('truetype'), url('static/343C58_0_0.svg#343C58_0_0') format('svg');
+    src: url('/static/343C58_0_0.eot?#iefix') format('embedded-opentype'), url('/static/343C58_0_0.woff') format('woff'), url('/static/343C58_0_0.ttf')  format('truetype'), url('/static/343C58_0_0.svg#343C58_0_0') format('svg');
   }
   
 `;
