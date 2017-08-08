7bf62b739bf906f4e926f6f65bf99687ce79e0fa
lzgrzebski <myczek18@gmail.com>
2017-08-08 22:05:39 +0200

    sourcemaps in heroku

--- src/next.config.js
+++ src/next.config.js
@@ -2,5 +2,6 @@ module.exports = {
   webpack: (config) => {
     config.devtool = 'inline-source-map';
     return config
-  }
+  },
+  sourceMaps: ({ dev }) => dev ? 'cheap-module-inline-source-map' : 'source-map'
 }
